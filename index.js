4bbbdca2cb0f5a9852759866e5e401d92759142e
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-05-21 18:25:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(musicians, instruments) {
   return array
 }
 
-function johnLennonFacts() {
+function johnLennonFacts(facts) {
   var i = 0
-  while(i < )
+  while(i < facts.legnth)
 }
